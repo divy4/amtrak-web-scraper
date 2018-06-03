9d47cbe9013bbdcc20f440300d7f9b3a9e323aeb
Dan Ivy <danivy4@gmail.com>
2018-06-04 01:58:12 +0500

    Sucessfully pulling train status page!

--- amtrakwebscraper.py
+++ amtrakwebscraper.py
@@ -8,14 +8,22 @@ except ImportError:
 
 if __name__ == '__main__':
     url = 'https://assistive.amtrak.com/h5/assistive/train-status'
-    post = {'action': 'searchTrainStatus',
+    header = {  'accept': 'text/html',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en-q=0.9',
+                'cache-control': 'max-age=0',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'upgrade-insecure-requests': '1'
+            }
+    form = {'action': 'searchTrainStatus',
             'radioSelect': 'arrivalTime',
             'wdf_trainNumber': '392',
             'wdf_destination': 'Chicago, IL',
             'unStCode_wdf_destination': 'CHI',
-            'departdisplay_train_number': '05/27/2018'
+            'departdisplay_train_number': '06/02/2018'
             }
-    response = requests.post(url, json=post)
-    parsedPage = BeautifulSoup(response.content)
-    status = parsedPage.body.find('div', attrs={'class': 'result-label'})
-    print(status)
+    response = requests.post(url, headers=header, data=form)
+    parsedPage = BeautifulSoup(response.content, 'html5lib')
+    status = parsedPage.find('div', {'class': 'result-content'})
+    print(parsedPage.getText())
+
